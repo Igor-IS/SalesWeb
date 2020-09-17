0c88222a016540078ff9f2fa4ebe17c37a5db846
Igor-IS <igorvulkan@gmail.com>
2020-09-17 15:08:02 -0300

    Returning custom error page

--- SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
+++ SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace SalesWebMvc.Models.ViewModels
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string Message { get; set; } // atributo criado para personalizar msg de erro
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
